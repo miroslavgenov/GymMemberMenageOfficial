e27555516fe7212f12b45cc5ac52965568dda2cb
miroslavgenov <buchvata123@abv.bg>
2022-07-29 17:01:16 +0300

    added main menu activity

--- app/src/main/java/com/miroslav/gymmembermenageofficial/MainActivity.java
+++ app/src/main/java/com/miroslav/gymmembermenageofficial/MainActivity.java
@@ -1,14 +1,62 @@
 package com.miroslav.gymmembermenageofficial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 
 import android.os.Bundle;
+
+import com.miroslav.gymmembermenageofficial.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
+
+
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
