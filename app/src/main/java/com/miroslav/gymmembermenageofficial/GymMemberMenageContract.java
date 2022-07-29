504bdf3a0af974fac197831411606ac1ffdd99b0
miroslavgenov <buchvata123@abv.bg>
2022-07-29 22:08:52 +0300

    add Constarct class

---
+++ app/src/main/java/com/miroslav/gymmembermenageofficial/GymMemberMenageContract.java
@@ -0,0 +1,66 @@
+package com.miroslav.gymmembermenageofficial;
+
+public static final class GymMemberMenageContract extends BaseColumn {
+	GymMemberMenageContract() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static final class ClientEntry{
+		public static final String TABLE_NAME="client"; 
+		public static final String COLUMN_NAME_CLIENT_ID="client_id";
+	}
+	
+	public static final class CardEnty{
+		public static final String TABLE_NAME="card";
+		public static final String COLUMN_NAME_CARD_ID="card_id";
+		
+	}
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
